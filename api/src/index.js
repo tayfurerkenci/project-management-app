7ba5f955a0e3f591c28d4b2a28305b8f8a758549
Tayfur Erkenci <terkenci@gmail.com>
2022-08-09 18:07:53 +0300

    apollo cache configured

--- api/src/index.js
+++ api/src/index.js
@@ -5,11 +5,14 @@ const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+const cors = require('cors');
 
 const app = express();
 
 // Connect to database
 connectDB();
+
+app.use(cors());
 
 app.use(
   '/graphql',
--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,29 @@
-import Header from './components/Header';
+import Header from './components/UI/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+import Clients from './components/Client/Clients';
+
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
 
 const client = new ApolloClient({
   uri: 'http://localhost:5000/graphql',
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 
 function App() {
@@ -12,7 +32,7 @@ function App() {
       <ApolloProvider client={client}>
         <Header />
         <div className="container">
-          <h1>Hello World</h1>
+          <Clients />
         </div>
       </ApolloProvider>
     </>
