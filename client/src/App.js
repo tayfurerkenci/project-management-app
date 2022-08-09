14887c3b87b04cd6e87247f1c41d20cff6a614ef
Tayfur Erkenci <terkenci@gmail.com>
2022-08-10 00:04:26 +0300

    i am so tired

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Header from './components/UI/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -36,6 +37,7 @@ function App() {
           <Header />
           <div className="container">
             <Routes>
+              <Route path="/projects/:id" element={<Project />} />
               <Route path="/" element={<Home />} />
               <Route path="*" element={<NotFound />} />
             </Routes>
